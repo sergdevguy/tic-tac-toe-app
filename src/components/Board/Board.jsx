38213648ef527d7aba36de915adb823e1b14ec75
Sergdevguy <sergdevguy@gmail.com>
2021-09-18 04:19:50 +0300

    Step 5. Add winner functionality

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -8,6 +8,9 @@ const Board = () => {
 
     const handleClick = (i) => {
         const squaresCopy = squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squaresCopy[i] = xIsNext ? 'X' : 'O';
         setSquares(squaresCopy);
         setXisNext(!xIsNext);
@@ -22,7 +25,13 @@ const Board = () => {
         );
     }
 
-    const status = 'Следующий ход: ' + (xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Выиграл ' + winner;
+    } else {
+        status = 'Следующий ход: ' + (xIsNext ? 'X' : 'O');
+    }
 
     return (
         <div>
@@ -47,3 +56,23 @@ const Board = () => {
 }
 
 export default Board;
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
